efba75ae347467afceaf96359350437d97b31325
Chris Mc <prince.chrismc@gmail.com>
2020-05-05 21:53:45 -0400

    Update test_sample.py

--- offline/test/test_sample.py
+++ offline/test/test_sample.py
@@ -1,7 +1,21 @@
+from conans.test.utils.tools import TestClient
+
 def func(x):
     return x + 1
 
-def test_answer():
-    assert func(3) == 5
+def test_requires():
+    conanfile = """from conans import ConanFile
+class MyConanfileBase(ConanFile):
+    def source(self):
+        self.output.info("My cool source!")
+    def build(self):
+        self.output.info("My cool build!")
+    def package(self):
+        self.output.info("My cool package!")
+    def package_info(self):
+        self.output.info("My cool package_info!")
+"""
+    client = TestClient()
+    client.save({"conanfile.py": conanfile})
+    client.run("export . MyConanfileBase/1.1@lasote/testing")
     assert func(3) == 4
-    
