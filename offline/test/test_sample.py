0ef1e65969e1576a9100f8c84f0038840e8a275e
Christopher McArthur <prince.chrismc@gmail.com>
2020-05-07 18:28:17 -0400

    and its testable

--- offline/test/test_sample.py
+++ offline/test/test_sample.py
@@ -1,8 +1,5 @@
 from conans.test.utils.tools import TestClient
 
-def func(x):
-    return x + 1
-
 def test_requires():
     client = TestClient()
     conanfile = """from conans import ConanFile
@@ -22,7 +19,7 @@ class MyConanfileBase(ConanFile):
     reuse = """from conans import python_requires
 base = python_requires("MyConanfileBase/1.1@lasote/testing")
 class PkgTest(base.MyConanfileBase):
-pass
+    pass
 """
 
     client.save({"conanfile.py": reuse}, clean_first=True)
@@ -31,3 +28,39 @@ pass
     assert("Pkg/0.1@lasote/testing: My cool build!" in client.out)
     assert("Pkg/0.1@lasote/testing: My cool package!" in client.out)
     assert("Pkg/0.1@lasote/testing: My cool package_info!" in client.out)
+
+def test_override_requires():
+    client = TestClient()
+    conanfile = """from conans import ConanFile
+class MyConanfileBase(ConanFile):
+    def source(self):
+        self.output.info("My base source!")
+    def build(self):
+        self.output.info("My base build!")
+    def package(self):
+        self.output.info("My base package!")
+    def package_info(self):
+        self.output.info("My base package_info!")
+"""
+    client.save({"conanfile.py": conanfile})
+    client.run("export . MyConanfileBase/1.1@lasote/testing")
+
+    reuse = """from conans import python_requires
+base = python_requires("MyConanfileBase/1.1@lasote/testing")
+class PkgTest(base.MyConanfileBase):
+    def source(self):
+        self.output.info("My pkg source!")
+    def build(self):
+        self.output.info("My pkg build!")
+    def package(self):
+        self.output.info("My pkg package!")
+    def package_info(self):
+        self.output.info("My pkg package_info!")
+"""
+
+    client.save({"conanfile.py": reuse}, clean_first=True)
+    client.run("create . Pkg/0.1@lasote/testing")
+    assert("Pkg/0.1@lasote/testing: My pkg source!" in client.out)
+    assert("Pkg/0.1@lasote/testing: My pkg build!" in client.out)
+    assert("Pkg/0.1@lasote/testing: My pkg package!" in client.out)
+    assert("Pkg/0.1@lasote/testing: My pkg package_info!" in client.out)
